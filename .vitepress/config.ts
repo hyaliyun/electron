f0bad1bfc6c7dbc2a2ad187100b0053b85ffdc8b
543x <hy0228@aliyun.com>
2024-09-17 01:26:02 +0800

    更新 config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -133,45 +133,45 @@ export default defineConfig({
       searchParameters: {
         facetFilters: ['tags:cn']
       },
-      placeholder: '搜索文档',
-      translations: {
-        button: {
-          buttonText: '搜索'
-        },
-        modal: {
-          searchBox: {
-            resetButtonTitle: '清除查询条件',
-            resetButtonAriaLabel: '清除查询条件',
-            cancelButtonText: '取消',
-            cancelButtonAriaLabel: '取消'
-          },
-          startScreen: {
-            recentSearchesTitle: '搜索历史',
-            noRecentSearchesText: '没有搜索历史',
-            saveRecentSearchButtonTitle: '保存到搜索历史',
-            removeRecentSearchButtonTitle: '从搜索历史中移除',
-            favoriteSearchesTitle: '收藏',
-            removeFavoriteSearchButtonTitle: '从收藏中移除'
-          },
-          errorScreen: {
-            titleText: '无法获取结果',
-            helpText: '你可能需要检查你的网络连接'
-          },
-          footer: {
-            selectText: '选择',
-            navigateText: '切换',
-            closeText: '关闭',
-            searchByText: '搜索供应商'
-          },
-          noResultsScreen: {
-            noResultsText: '无法找到相关结果',
-            suggestedQueryText: '你可以尝试查询',
-            reportMissingResultsText: '你认为这个查询应该有结果？',
-            reportMissingResultsLinkText: '向我们反馈'
-          }
-        }
-      },
-    },
+      placeholder: 'Search documents',
+translations: {
+button: {
+buttonText: 'Search'
+},
+modal: {
+searchBox: {
+resetButtonTitle: 'Clear search criteria',
+resetButtonAriaLabel: 'Clear search criteria',
+cancelButtonText: 'Cancel',
+cancelButtonAriaLabel: 'Cancel'
+},
+startScreen: {
+recentSearchesTitle: 'Search history',
+noRecentSearchesText: 'No search history',
+saveRecentSearchButtonTitle: 'Save to search history',
+removeRecentSearchButtonTitle: 'Remove from search history',
+favoriteSearchesTitle: 'Favorites',
+removeFavoriteSearchButtonTitle: 'Remove from favorites'
+},
+errorScreen: {
+titleText: 'Unable to retrieve results',
+helpText: 'You may need to check your network connection'
+},
+footer: {
+selectText: 'Select',
+navigateText: 'Toggle',
+closeText: 'Close',
+searchByText: 'Search for suppliers'
+},
+noResultsScreen: {
+noResultsText: 'No relevant results found',
+suggestedQueryText: 'You can try a query',
+reportMissingResultsText: 'Do you think this query should have results? ',
+reportMissingResultsLinkText: 'Give us feedback'
+}
+}
+},
+},
 
 
     footer: {
